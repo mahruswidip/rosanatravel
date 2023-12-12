cab5568ba25f42ab53ab42c42378de7d614b7a0d
Mahrus <mahrusrama7@gmail.com>
2023-12-12 16:22:07 +0700

    Update landing.php

--- application/views/landing.php
+++ application/views/landing.php
@@ -107,7 +107,7 @@
         </div>
     </div>
 </div>
-
+<!-- 
 <div class="untree_co-section count-numbers py-5">
     <div class="container">
         <div class="row">
@@ -116,7 +116,7 @@
                     <div class="counter">
                         <span class="" data-number="9313">0</span>
                     </div>
-                    <span class="caption">No. of Travels</span>
+                    <span class="caption">Jumlah Keberangkatan</span>
                 </div>
             </div>
             <div class="col-6 col-sm-6 col-md-6 col-lg-3">
@@ -124,7 +124,7 @@
                     <div class="counter">
                         <span class="" data-number="8492">0</span>
                     </div>
-                    <span class="caption">No. of Clients</span>
+                    <span class="caption">Jumlah Paket Aktif</span>
                 </div>
             </div>
             <div class="col-6 col-sm-6 col-md-6 col-lg-3">
@@ -145,7 +145,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 
 <style>
     .ellipsis {
@@ -364,3 +364,65 @@
         </div>
     </div>
 </div>
+
+<section id="contact" class="contact py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4" data-aos="fade-right">
+                <div class="section-title">
+                    <h2>Kontak Kami</h2>
+                </div>
+                <p class="py-4">Temukan Dukungan untuk Kebutuhan Perjalanan Anda. Dapatkan jawaban dan solusi atas pertanyaan Anda di sini. Kami selalu siap untuk membantu Anda.</p>
+            </div>
+
+            <div class="col-lg-8" data-aos="fade-up" data-aos-delay="100">
+                <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.168803059516!2d112.90037107579782!3d-7.6649936758579384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7cf567c652c2d%3A0x168233139d4ce4fd!2sRosana%20Travel%20Pasuruan%20(Official)!5e0!3m2!1sen!2sid!4v1702372637803!5m2!1sen!2sid" frameborder="0" allowfullscreen></iframe>
+                <div class="info mt-4">
+                    <i class="bi bi-geo-alt"></i>
+                    <h4>Kantor Pusat :</h4>
+                    <p>Jl. Dokter Setiabudi No.20, Purutrejo, Kec. Purworejo, Kota Pasuruan, Jawa Timur 67117</p>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 mt-4">
+                        <div class="info">
+                            <i class="bi bi-envelope"></i>
+                            <h4>Email :</h4>
+                            <p>info@rosanatourtravel.com</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="info w-100 mt-4">
+                            <i class="bi bi-phone"></i>
+                            <h4>Nomor HP / Whatsapp :</h4>
+                            <p>+62 811 3003 258</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- 
+                <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+                        </div>
+                        <div class="col-md-6 form-group mt-3 mt-md-0">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+                        </div>
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+                    </div>
+                    <div class="form-group mt-3">
+                        <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                    </div>
+                    <div class="my-3">
+                        <div class="loading">Loading</div>
+                        <div class="error-message"></div>
+                        <div class="sent-message">Your message has been sent. Thank you!</div>
+                    </div>
+                    <div class="text-center"><button type="submit">Send Message</button></div>
+                </form> -->
+            </div>
+        </div>
+
+    </div>
+</section><!-- End Contact Section -->
