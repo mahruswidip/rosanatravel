5ed39ce218494054a710505b2fce8f55edc0a90f
Mahrus <mahrusrama7@gmail.com>
2024-02-16 08:26:59 +0700

    Update landing.php

--- application/views/landing.php
+++ application/views/landing.php
@@ -462,7 +462,7 @@
 <!-- Bootstrap CSS -->
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="..."> -->
 <!-- Bootstrap JS -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="..."></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="..."></script> -->
 
 <script>
     // Ambil elemen gambar dan loading
