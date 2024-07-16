50fc482eb37135733eabc5a6a9980d180db465e0
Mahrus <mahrusrama7@gmail.com>
2024-07-16 08:23:29 +0700

    haji page

--- application/views/paket/haji.php
+++ application/views/paket/haji.php
@@ -1,42 +1,218 @@
-<div class="container" style="margin-top: 150px;">
-    <div class="row justify-content-center text-center mb-5">
-        <div class="col-lg-6">
-            <h2 class="section-title text-center mb-3">Haji Eksklusif</h2>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<div class="hero hero-inner">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-7 mx-auto text-center">
+                <div class="intro-wrap">
+                    <h1 class="mb-0">Haji Eksklusif</h1>
+                    <br>
+                    <h5 class="text-white" style="font-family: 'Source Serif Pro', serif;">Nikmati Ibadah Haji dengan Fasilitas Bintang 5 dan Pengalaman Tak Tertandingi Bersama Rosana Travel</h5>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row justify-content-center text-center mb-5">
-        <div class="col-lg-6">
-            <h2 class="section-title text-center mb-3">Pembanding Paket Haji Kami</h2>
+</div>
+
+<div class="untree_co-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="service text-center">
+                    <span class="icon-hotel"></span>
+                    <h3>Kenyamanan Bintang 5</h3>
+                    <p>Pengalaman Menginap di Hotel Mewah Dekat Masjid Suci.</p>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="service text-center">
+                    <span class="icon-user"></span>
+                    <h3>Bimbingan Spiritual Profesional</h3>
+                    <p>Didampingi oleh Pembimbing Ibadah Bersertifikasi Nasional.</p>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="service text-center">
+                    <span class="icon-book"></span>
+                    <h3>Ilmu dan Iman yang Mendalam</h3>
+                    <p>Program Pesantren Haji untuk Meningkatkan Pemahaman Agama.</p>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="service text-center">
+                    <span class="icon-tag"></span>
+                    <h3>Perjalanan Terjangkau</h3>
+                    <p>Paket Haji Eksklusif dengan Harga Kompetitif dan Fasilitas Terbaik.</p>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <!-- <?php foreach ($paketumroh as $paket_item) : ?>
-            <a href="<?php echo base_url('paket/detail_paket/' . $paket_item['id_paket']); ?>">
-                <div class="col-6 col-sm-6 col-md-6 col-lg-3 mb-4 mb-lg-0">
-                    <div class="media-1">
-                        <img src="https://alfatihahtravel.com/admin/assets/images/<?php echo $paket_item['paket_img']; ?>" alt="Image" class="img-fluid mb-4" style="box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);">
-                        <span class="d-flex align-items-center loc mb-2">
-                            <span class="icon-room mr-3"></span>
-                            <span><?php echo $paket_item['kategori']; ?></span>
-                        </span>
-                        <div class="d-flex align-items-center">
-                            <div>
-                                <h3 class="ellipsis"><?php echo $paket_item['nama_program']; ?></h3>
-                                <div class="price ml-auto">
-                                    <span>DP Mulai - Rp. <?php echo number_format($paket_item['uang_muka'], 0, ',', '.'); ?></span>
-                                </div>
-                            </div>
+</div>
 
+<div class="untree_co-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="media-1">
+                    <a href="#" class="d-block mb-3"><img src="<?php echo base_url('assets/') ?>images/pagehaji/fullhotel.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="d-flex">
+                        <div>
+                            <h3><a href="#">Fasilitas Full Hotel Bintang 5 Tanpa Hotel Transit</a></h3>
+                            <p>Kenyamanan Menginap di Jarak Nol Meter dari Masjid Nabawi dan Masjidil Haram</p>
                         </div>
-                        <div class="row align-items-center py-3">
-                            <div class="col-sm-12 col-md-6 mb-3 col-lg-8">
-                                <a href="<?php echo base_url('paket/detail_paket/' . $paket_item['id_paket']); ?>" class="btn btn-primary btn-block">Detail</a>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
-            </a>
-        <?php endforeach; ?> -->
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="media-1">
+                    <a href="#" class="d-block mb-3"><img src="<?php echo base_url('assets/') ?>images/pagehaji/kyaihuda2.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="d-flex">
+                        <div>
+                            <h3><a href="#">Bimbingan Ibadah yang Profesional</a></h3>
+                            <p>Didampingi Muthawwif dan Pembimbing Ibadah Bersertifikasi Nasional</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="media-1">
+                    <a href="#" class="d-block mb-3"><img src="<?php echo base_url('assets/') ?>images/pagehaji/pesantren.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="d-flex">
+                        <div>
+                            <h3><a href="#">Program Pesantren Haji Eksklusif</a></h3>
+                            <p>Meningkatkan Ilmu Agama dan Iman Selama Perjalanan Haji</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-md-6 col-lg-3">
+                <div class="media-1">
+                    <a href="#" class="d-block mb-3"><img src="<?php echo base_url('assets/') ?>images/pagehaji/murah.jpg" alt="Image" class="img-fluid"></a>
+                    <div class="d-flex">
+                        <div>
+                            <h3><a href="#">Tarif Terjangkau dengan Kualitas Terbaik</a></h3>
+                            <p>Paket Haji Eksklusif dengan Harga Murah di Kelasnya</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="untree_co-section">
+    <div class="container">
+        <div class="row mb-5 justify-content-center">
+            <div class="col-lg-9 text-center">
+                <h2 class="section-title text-center mb-3">Apa yang dimaksud FULL HOTEL ?</h2>
+                <h5 style="font-family: 'Source Serif Pro', serif;">Nikmati Kenyamanan beribadah Tanpa ber-Pindah ke Rumah Transit / Hotel Transit. Hotel yang Terletak Strategis Dekat dengan Masjidil Haram dan Masjid Nabawi untuk Pengalaman Ibadah yang Lebih Khusyuk</h5>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 order-lg-1">
+                <div class="h-100">
+                    <div class="frame h-100">
+                        <img src="<?php echo base_url('assets/') ?>images/pagehaji/hajipaket.jpg" alt="Haji Paket" style="width: 100%; object-fit: contain;">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 order-lg-2">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="feature-1 d-md-flex">
+                            <div class="align-self-center">
+                                <span class="fas fa-kaaba display-5 text-primary mb-4"></span>
+                                <h3>Wukuf di Arafah</h3>
+                                <p class="mb-0">Kami menempatkan prioritas utama pada ibadah yang paling krusial dalam pelaksanaan haji, yakni wukuf di Arafah pada tanggal 9 Dzulhijjah, merupakan bagian tak terpisahkan dari rukun haji yang memperdalam makna dan keberkahan perjalanan spiritual jamaah.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="feature-1 d-md-flex">
+                            <div class="align-self-center">
+                                <span class="fas fa-book display-5 text-primary mb-4"></span>
+                                <h3>Program Pesantren Haji</h3>
+                                <p class="mb-0">Kami menyediakan program pesantren haji yang membekali para jamaah dengan ilmu agama, mempersiapkan mereka untuk pelaksanaan ibadah haji yang lebih khusyuk dan bermakna.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="feature-1 d-md-flex">
+                            <div class="align-self-center">
+                                <span class="fas fa-hotel display-5 text-primary mb-4"></span>
+                                <h3>Full Hotel Bintang 5</h3>
+                                <p class="mb-0">Menginap di hotel bintang 5 tanpa pindah ke hotel transit, memastikan kenyamanan dan kemudahan akses ke Masjidil Haram.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="feature-1 d-md-flex">
+                            <div class="align-self-center">
+                                <span class="fas fa-mosque display-5 text-primary mb-4"></span>
+                                <h3>Akses Mudah ke Masjidil Haram</h3>
+                                <p class="mb-0">Dengan hotel yang dekat, Anda bisa beribadah di Masjidil Haram kapan saja tanpa khawatir jarak jauh atau biaya transportasi.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12">
+                        <div class="feature-1 d-md-flex">
+                            <div class="align-self-center">
+                                <span class="fas fa-money-bill-wave display-5 text-primary mb-4"></span>
+                                <h3>Biaya Efisien</h3>
+                                <p class="mb-0">Tidak perlu khawatir biaya membengkak akibat transportasi, kami menyediakan akomodasi yang nyaman dan terjangkau.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="untree_co-section">
+    <div class="container">
+        <div class="row mb-5 justify-content-center">
+            <div class="col-lg-9 text-center">
+                <h2 class="section-title text-center mb-3">Pembimbing Ibadah Haji Bersertifikasi Nasional</h2>
+                <h5 style="font-family: 'Source Serif Pro', serif;">Didampingi oleh Pembimbing Haji Berpengalaman dan Bersertifikasi Nasional untuk Perjalanan Ibadah Haji Anda yang Lebih Bermakna</h5>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 order-lg-1">
+                <div class="row">
+
+                </div>
+            </div>
+
+            <div class="col-lg-6 order-lg-2">
+                <div class="row">
+
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
+
+<div class="py-5 cta-section">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h2 class="mb-2 text-white">Lets you Explore the Best. Contact Us Now</h2>
+                <p class="mb-4 lead text-white text-white-opacity">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, fugit?</p>
+                <p class="mb-0"><a href="booking.html" class="btn btn-outline-white text-white btn-md font-weight-bold">Get in touch</a></p>
+            </div>
+        </div>
     </div>
 </div>
