9aeea1a043de48c86ffca68747f6f0f393672fb9
Mahrus <mahrusrama7@gmail.com>
2024-07-26 09:56:19 +0700

    main

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -64,7 +64,7 @@
     <nav class="site-nav">
         <div class="container">
             <div class="site-navigation">
-                <a href="<?php echo base_url('') ?>" class="logo m-0"><img style="height: 3.8rem;" src="<?php echo base_url('assets/images/logo/logoroswhite.png') ?>" alt=""></a>
+                <a href="<?php echo base_url('') ?>" class="logo m-0"><img style="height: 3.8rem;" src="<?php echo base_url('assets/images/logo/logoros.png') ?>" alt=""></a>
                 <ul class="js-clone-nav d-none d-lg-inline-block text-left site-menu float-right">
                     <li class="active"><a href="<?php echo base_url('') ?>">Beranda</a></li>
                     <li class="has-children">
@@ -166,12 +166,12 @@
             <div class="container">
                 <div class="row text-center">
                     <div class="col-md-8 mb-3 mb-md-0 mx-auto">
-                        <p>Copyright &copy;
+                        <!-- <p>Copyright &copy;
                             <script>
                                 document.write(new Date().getFullYear());
                             </script>. All Rights Reserved. &mdash;
-                            Designed with love by <a href="https://untree.co" class="link-highlight">Untree.co</a>
-                            <!-- License information: https://untree.co/license/ -->Distributed By <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                            Designed with love by <a href="https://untree.co" class="link-highlight">Untree.co</a> 
+                            License information: https://untree.co/license/ Distributed By <a href="https://themewagon.com" target="_blank">ThemeWagon</a>-->
                         </p>
                     </div>
 
