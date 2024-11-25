625da232e170a76e5d461dd9a4a9f8ff92b12eb7
mahruswidip <mahrusrama7@gmail.com>
2024-11-25 11:27:00 +0700

    Update confirmation.php

--- application/views/pemesanan/confirmation.php
+++ application/views/pemesanan/confirmation.php
@@ -22,45 +22,49 @@
     display: none;
 }
 </style><!-- Detail Paket Section -->
+<?php 
+    $registration_data = $this->session->userdata('registration_data');
+?>
 
 <div class="untree_co-section" style="padding-top: 170px!important;">
     <div class="container">
         <div class="row">
             <div class="col">
                 <div class="alert alert-success py-4" role="alert">
-                    <h3 class="alert-heading">Pendaftaran Berhasil !</h3>
+                    <h3 class="alert-heading">Pendaftaran Berhasil!</h3>
                     <p>Kami akan segera menghubungi anda melalui Nomor Whatsapp:
-                        <strong><?php echo $this->session->userdata('nomor_telepon') ?></strong>. Pendaftaran dan
-                        pemilihan paket anda telah berhasil dilakukan. <br> Simpan QR Code ini dan tunjukkan kepada tim
-                        marketing kami.
+                        <strong><?php echo $registration_data['nomor_telepon']; ?></strong>. Pendaftaran dan pemilihan
+                        paket anda telah berhasil dilakukan. <br> Simpan QR Code ini dan tunjukkan kepada tim marketing
+                        kami.
                     </p>
                 </div>
             </div>
         </div>
         <div class="row mx-3 mt-4">
             <div class="col-lg-7">
-                <div class="custom-block" data-aos="fade-up" data-aos-delay="100">
+                <div class="custom-block">
                     <h2 class="section-title">Konfirmasi Pemesanan</h2>
                     <br>
-                    <p>UUID : <strong></strong></p>
-                    <p>NIK : <strong></strong></p>
-                    <p>Nama Pendaftar : <strong></strong></p>
-                    <p>Jenis Kelamin : <strong></strong></p>
-                    <p>Nomor Telepon : <strong></strong></p>
-                    <p>Alamat : <strong></strong></p>
-                    <p>Email : <strong></strong></p>
+                    <p>UUID : <strong><?php echo $registration_data['uuid'] ?? 'Tidak tersedia'; ?></strong></p>
+                    <p>NIK : <strong><?php echo $registration_data['nik']; ?></strong></p>
+                    <p>Nama Pendaftar : <strong><?php echo $registration_data['nama_pendaftar']; ?></strong></p>
+                    <p>Jenis Kelamin : <strong><?php echo $registration_data['jenis_kelamin']; ?></strong></p>
+                    <p>Nomor Telepon : <strong><?php echo $registration_data['nomor_telepon']; ?></strong></p>
+                    <p>Alamat : <strong><?php echo $registration_data['alamat']; ?></strong></p>
+                    <p>Email : <strong><?php echo $registration_data['email']; ?></strong></p>
                     <h6 class="font-weight-bold mt-2">Detail Paket yang dipesan</h6>
-                    <p class=""></p>
-                    <p class="">Rencana Berangkat : ?> Orang</p>
+                    <p class=""><?php echo $registration_data['pesan_apa']; ?></p>
+                    <p class="">Rencana Berangkat : <?php echo $registration_data['berapa_orang']; ?> Orang</p>
                 </div>
             </div>
             <div class="col-lg-3">
-                <img src="<?php echo base_url('assets/') ?>images/qrcodependaftar/<?php echo $this->session->userdata('uuid') . '.png' ?>"
-                    alt="">
+                <img src="<?php echo base_url('assets/images/qrcodependaftar/') . $registration_data['uuid'] . '.png'; ?>"
+                    alt="QR Code">
             </div>
         </div>
     </div>
 </div>
+
 
 <script>
 
