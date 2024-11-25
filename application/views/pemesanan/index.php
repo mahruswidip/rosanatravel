730aca26e9ece25bb533cfc23dba88dd26f7543d
mahruswidip <mahrusrama7@gmail.com>
2024-11-25 10:51:37 +0700

    Update index.php

--- application/views/pemesanan/index.php
+++ application/views/pemesanan/index.php
@@ -182,6 +182,7 @@
                                 width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
                                 referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </section>
+                        <button class="btn btn-primary">Pesan Sekarang</button>
                     </form>
                 </div>
             </div>
