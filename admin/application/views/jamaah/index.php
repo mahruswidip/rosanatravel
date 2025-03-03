06e658340a61fd817439844f43ea49970802bcad
Mahrus <mahrusrama7@gmail.com>
2025-03-03 12:30:52 +0700

    Update index.php

--- admin/application/views/jamaah/index.php
+++ admin/application/views/jamaah/index.php
@@ -17,6 +17,8 @@
                         <table id="dataTable-jamaah" class="table align-items-center mb-0">
                             <thead>
                                 <tr>
+                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7" style="display: none;">
+                                        ID Jamaah</th>
                                     <th
                                         class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                         Foto</th>
@@ -86,26 +88,26 @@
     </div>
 </div>
 <style>
-#dataTable-jamaah {
-    height: 300px !important;
-}
+    #dataTable-jamaah {
+        height: 300px !important;
+    }
 
-#dataTable-jamaah td:nth-child(4) {
-    width: 50px;
-    /* Set your desired width */
-}
+    #dataTable-jamaah td:nth-child(4) {
+        width: 50px;
+        /* Set your desired width */
+    }
 
-#dataTable-jamaah tbody tr:hover {
-    background-color: #f5f5f5;
-    /* Change the background color to your desired hover color */
-    cursor: pointer;
-    /* Change the cursor to a pointer on hover */
-}
+    #dataTable-jamaah tbody tr:hover {
+        background-color: #f5f5f5;
+        /* Change the background color to your desired hover color */
+        cursor: pointer;
+        /* Change the cursor to a pointer on hover */
+    }
 
-.long-text {
-    max-width: 200px;
-    white-space: pre-wrap;
-}
+    .long-text {
+        max-width: 200px;
+        white-space: pre-wrap;
+    }
 </style>
 <!-- Include jQuery and DataTables libraries -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -113,188 +115,192 @@
 
 <!-- DataTable initialization with AJAX -->
 <script type="text/javascript">
-$(document).ready(function() {
-    function generateStarRating(starCount) {
-        var stars = '';
-        for (var i = 0; i < starCount; i++) {
-            stars += '&#11088;'; // Unicode character for a star icon
+    $(document).ready(function() {
+        function generateStarRating(starCount) {
+            var stars = '';
+            for (var i = 0; i < starCount; i++) {
+                stars += '&#11088;'; // Unicode character for a star icon
+            }
+            return stars;
         }
-        return stars;
-    }
-    $('#dataTable-jamaah').DataTable({
-        "language": {
-            "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
-            "paginate": {
-                "previous": "<",
-                "next": ">"
+        $('#dataTable-jamaah').DataTable({
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
+                "paginate": {
+                    "previous": "<",
+                    "next": ">"
+                },
             },
-        },
-        "pageLength": 10,
-        ajax: {
-            url: '<?php echo base_url("jamaah/view"); ?>',
-            type: 'POST',
-            dataSrc: ''
-        },
-        rowCallback: function(row, data) {
-            // Attach click event listener to each row
-            $(row).on('click', function() {
-                // Get the ID of the clicked row's data
-                var jamaahId = data.id_jamaah;
+            "pageLength": 10,
+            ajax: {
+                url: '<?php echo base_url("jamaah/view"); ?>',
+                type: 'POST',
+                dataSrc: ''
+            },
+            rowCallback: function(row, data) {
+                // Attach click event listener to each row
+                $(row).on('click', function() {
+                    // Get the ID of the clicked row's data
+                    var jamaahId = data.id_jamaah;
 
-                // Redirect to the detail page using the jamaahId
-                window.location.href = '<?php echo site_url('jamaah/detail/') ?>' +
-                jamaahId;
-            });
-        },
-        columns: [{
-                data: 'jamaah_img',
-                render: function(data, type, row) {
-                    // Assuming data contains the filename (e.g., "c39f997660f4a7e707658a141d30e7ce.jpg")
-                    // You need to provide the correct path to your image folder.
-                    var imagePath = '<?php echo base_url("assets/images/"); ?>' + data;
-                    // Generate HTML for displaying the image
-                    return '<img src="' + imagePath +
-                        '" alt="Image" class="img-fluid border-radius-lg" style="max-width: 100px; max-height: 100px;">';
-                }
+                    // Redirect to the detail page using the jamaahId
+                    window.location.href = '<?php echo site_url('jamaah/detail/') ?>' +
+                        jamaahId;
+                });
             },
-            {
-                data: 'null',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0">' + row.nama_jamaah +
-                        '</p>' + '<p class="text-xs text-secondary mb-0">' + row.nomor_paspor +
-                        '</p>';
+            columns: [{
+                    data: 'id_jamaah',
+                    visible: false
                 },
-            },
-            {
-                data: 'null',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0">' + row.nomor_telepon +
-                        '</p>';
-                },
-            },
-            {
-                data: 'null',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0 long-text">' + row.alamat +
-                        '</p>';
-                },
-            },
-            {
-                data: 'qr_code_benar',
-                render: function(data, type, row) {
-                    // Assuming data contains the filename (e.g., "c39f997660f4a7e707658a141d30e7ce.jpg")
-                    // You need to provide the correct path to your image folder.
-                    var imagePath = '<?php echo base_url("assets/images/qr_uuid/"); ?>' + data;
-                    // Generate HTML for displaying the image
-                    if (data == "") {
-                        return '<a href="<?php echo base_url() . 'jamaah/updateqr/' ?>' + row
-                            .id_jamaah + '"' +
-                            'class="btn btn-dark btn-sm"><span class="fa fa-qrcode"></span></a>';
-                    } else {
+                {
+                    data: 'jamaah_img',
+                    render: function(data, type, row) {
+                        // Assuming data contains the filename (e.g., "c39f997660f4a7e707658a141d30e7ce.jpg")
+                        // You need to provide the correct path to your image folder.
+                        var imagePath = '<?php echo base_url("assets/images/"); ?>' + data;
+                        // Generate HTML for displaying the image
                         return '<img src="' + imagePath +
                             '" alt="Image" class="img-fluid border-radius-lg" style="max-width: 100px; max-height: 100px;">';
                     }
-                }
-            },
-            {
-                data: 'null',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0">' + row.created_att +
-                    '</p>';
                 },
-            },
-            {
-                data: null,
-                className: 'td-column-right',
-                render: function(data, type, row) {
-                    var userButton = '<a href="<?php echo site_url('jamaah/buatuser/'); ?>' +
-                        row.id_jamaah +
-                        '" class="btn bg-gradient-primary btn-sm"><span class="fas fa-user"></span>Buat User</a>';
-                    var editButton = '<a href="<?php echo site_url('jamaah/edit/'); ?>' + row
-                        .id_jamaah +
-                        '" class="btn bg-gradient-info btn-sm"><span class="fa fa-pencil"></span>Edit</a>';
-                    var deleteButton = '<a href="<?php echo site_url('jamaah/remove/'); ?>' +
-                        row.id_jamaah +
-                        '" class="btn bg-gradient-danger btn-sm"><span class="fa fa-trash"></span>Hapus</a>';
-                    return userButton + ' ' + editButton + ' ' + deleteButton;
-                }
-            },
+                {
+                    data: 'null',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0">' + row.nama_jamaah +
+                            '</p>' + '<p class="text-xs text-secondary mb-0">' + row.nomor_paspor +
+                            '</p>';
+                    },
+                },
+                {
+                    data: 'null',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0">' + row.nomor_telepon +
+                            '</p>';
+                    },
+                },
+                {
+                    data: 'null',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0 long-text">' + row.alamat +
+                            '</p>';
+                    },
+                },
+                {
+                    data: 'qr_code_benar',
+                    render: function(data, type, row) {
+                        // Assuming data contains the filename (e.g., "c39f997660f4a7e707658a141d30e7ce.jpg")
+                        // You need to provide the correct path to your image folder.
+                        var imagePath = '<?php echo base_url("assets/images/qr_uuid/"); ?>' + data;
+                        // Generate HTML for displaying the image
+                        if (data == "") {
+                            return '<a href="<?php echo base_url() . 'jamaah/updateqr/' ?>' + row
+                                .id_jamaah + '"' +
+                                'class="btn btn-dark btn-sm"><span class="fa fa-qrcode"></span></a>';
+                        } else {
+                            return '<img src="' + imagePath +
+                                '" alt="Image" class="img-fluid border-radius-lg" style="max-width: 100px; max-height: 100px;">';
+                        }
+                    }
+                },
+                {
+                    data: 'null',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0">' + row.created_att +
+                            '</p>';
+                    },
+                },
+                {
+                    data: null,
+                    className: 'td-column-right',
+                    render: function(data, type, row) {
+                        var userButton = '<a href="<?php echo site_url('jamaah/buatuser/'); ?>' +
+                            row.id_jamaah +
+                            '" class="btn bg-gradient-primary btn-sm"><span class="fas fa-user"></span>Buat User</a>';
+                        var editButton = '<a href="<?php echo site_url('jamaah/edit/'); ?>' + row
+                            .id_jamaah +
+                            '" class="btn bg-gradient-info btn-sm"><span class="fa fa-pencil"></span>Edit</a>';
+                        var deleteButton = '<a href="<?php echo site_url('jamaah/remove/'); ?>' +
+                            row.id_jamaah +
+                            '" class="btn bg-gradient-danger btn-sm"><span class="fa fa-trash"></span>Hapus</a>';
+                        return userButton + ' ' + editButton + ' ' + deleteButton;
+                    }
+                },
 
-        ],
-        "order": [
-            [6, 'asc']
-        ] // Menyortir berdasarkan kolom dengan indeks 1 (kolom is_aktif) secara descending (nilai 1 akan berada di atas)
+            ],
+            "order": [
+                [0, 'desc']
+            ] // Menyortir berdasarkan kolom dengan indeks 1 (kolom is_aktif) secara descending (nilai 1 akan berada di atas)
+        });
     });
-});
 </script>
 <!--untuk isi pada tabel ucapan selamat ulang tahun -->
 <script type="text/javascript">
-$(document).ready(function() {
-    $('#dataTable-ultah').DataTable({
-        "language": {
-            "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
-            "paginate": {
-                "previous": "<",
-                "next": ">"
+    $(document).ready(function() {
+        $('#dataTable-ultah').DataTable({
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.11.5/i18n/Indonesian.json",
+                "paginate": {
+                    "previous": "<",
+                    "next": ">"
+                },
             },
-        },
-        "pageLength": 10,
-        ajax: {
-            url: '<?php echo base_url("jamaah/view_ultah"); ?>',
-            type: 'POST',
-            dataSrc: ''
-        },
-        columns: [{
-                data: 'jamaah_img',
-                render: function(data, type, row) {
-                    var imagePath = '<?php echo base_url("assets/images/"); ?>' + data;
-                    return '<img src="' + imagePath + '" alt="Foto" class="img-fluid border-radius-lg" style="max-width: 100px; max-height: 100px;">';
-                }
+            "pageLength": 10,
+            ajax: {
+                url: '<?php echo base_url("jamaah/view_ultah"); ?>',
+                type: 'POST',
+                dataSrc: ''
             },
-            {
-                data: 'nama_jamaah',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
-                }
-            },
-            {
-                data: 'ttl',
-                render: function(data, type, row) {
-                    if (!data || data === '0000-00-00') {
-                        return 'Data tidak valid';
+            columns: [{
+                    data: 'jamaah_img',
+                    render: function(data, type, row) {
+                        var imagePath = '<?php echo base_url("assets/images/"); ?>' + data;
+                        return '<img src="' + imagePath + '" alt="Foto" class="img-fluid border-radius-lg" style="max-width: 100px; max-height: 100px;">';
                     }
-                    var dateParts = data.split("-");
-                    var tahun = dateParts[0];
-                    var bulan = parseInt(dateParts[1], 10) - 1;
-                    var tanggal = dateParts[2];
+                },
+                {
+                    data: 'nama_jamaah',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
+                    }
+                },
+                {
+                    data: 'ttl',
+                    render: function(data, type, row) {
+                        if (!data || data === '0000-00-00') {
+                            return 'Data tidak valid';
+                        }
+                        var dateParts = data.split("-");
+                        var tahun = dateParts[0];
+                        var bulan = parseInt(dateParts[1], 10) - 1;
+                        var tanggal = dateParts[2];
 
-                    var bulanNama = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+                        var bulanNama = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 
-                    return tanggal + ' ' + bulanNama[bulan] + ' ' + tahun;
-                }
-            },
-            {
-                data: 'nomor_telepon',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
-                }
-            },
-            {
-                data: 'alamat',
-                render: function(data, type, row) {
-                    return '<p class="text-xs font-weight-bold mb-0 long-text">' + data + '</p>';
-                }
-            },
-            {
-                data: null,
-                render: function() {
-                    return '';
-                }
-            },
-        ],
-        "order": [
-            [3, 'asc']
-        ]
+                        return tanggal + ' ' + bulanNama[bulan] + ' ' + tahun;
+                    }
+                },
+                {
+                    data: 'nomor_telepon',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0">' + data + '</p>';
+                    }
+                },
+                {
+                    data: 'alamat',
+                    render: function(data, type, row) {
+                        return '<p class="text-xs font-weight-bold mb-0 long-text">' + data + '</p>';
+                    }
+                },
+                {
+                    data: null,
+                    render: function() {
+                        return '';
+                    }
+                },
+            ],
+            "order": [
+                [3, 'asc']
+            ]
+        });
     });
-});
 </script>
